85cd9322d26b01af76b1624f5b6b71aab148669e
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-09-23 17:45:11 +0900

    240923-s01

--- src/condition/Switch2.java
+++ src/condition/Switch2.java
@@ -8,12 +8,12 @@ public class Switch2 {
         // 선택된 데이터를 반환할 수 있음
         int grade = 0;
 
-        int coupon = switch (grade) {
-            case 1 -> 1000;
-            case 2 -> 2000;
-            case 3 -> 3000;
-            default -> 500;
-        };
-        System.out.println("발급받은 쿠폰: " + coupon + "원");
+//        int coupon = switch (grade) {
+//            case 1 -> 1000;
+//            case 2 -> 2000;
+//            case 3 -> 3000;
+//            default -> 500;
+//        };
+//        System.out.println("발급받은 쿠폰: " + coupon + "원");
     }
 }
