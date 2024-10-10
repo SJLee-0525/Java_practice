b7342cb760d325a45e06298ab3ae5af82f7cbfa9
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-10-11 01:17:59 +0900

    241010-h02

--- src/ssafy/testa/SweaCafe.java
+++ src/ssafy/testa/SweaCafe.java
@@ -15,8 +15,11 @@ public class SweaCafe {
 
     private static int[] di = new int[]{1, 1, -1, -1};
     private static int[] dj = new int[]{1, -1, -1, 1};
+    private static int[] ate;
 
     private static int[][] cafeRoad;
+    private static int[][] visited;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,71 +41,60 @@ public class SweaCafe {
             result = 0;
             for (int i = 0; i < N - 2; i++) {
                 for (int j = 1; j < N - 1; j++) {
-                    int[] ate = new int[101];
-                    int[][] visited = new int[N][N];
-                    exploreCafe(i, j, i, j, 0, ate, visited);
+                    ate = new int[101];
+                    visited = new int[N][N];
+                    exploreCafe(i, j, i, j, 0);
                 }
             }
 
-            if (result == 0) {
-                result = -1;
-            }
+            if (result == 0) result = -1;
 
             sb.append('#').append(tc).append(' ').append(result).append('\n');
             bw.write(sb.toString());
             bw.flush();
             sb.setLength(0);
         }
+
         br.close();
         bw.close();
     }
 
-    private static void exploreCafe(int i, int j, int si, int sj, int k, int[] inputAte, int[][] inputVisited) {
+    private static void exploreCafe(int i, int j, int si, int sj, int k) {
         if (k > 0 && i == si && j == sj) {
-//            System.out.println(Arrays.toString(inputAte));
-            int temp = Arrays.stream(inputAte).sum();
-            if (result < temp) {
-                result = temp;
-            }
+            int temp = Arrays.stream(ate).sum();
+            if (result < temp) result = temp;
             return;
         }
 
         if (k >= 4 || i < si) return;
 
-//        System.out.println(i + ", " + j + ", " + si + ", " + sj);
-
         int mi = i + di[k];
         int mj = j + dj[k];
         if (0 <= mi && mi < N &&
                 0 <= mj && mj < N &&
-                inputVisited[mi][mj] == 0 &&
-                inputAte[cafeRoad[mi][mj]] == 0) {
+                visited[mi][mj] == 0 &&
+                ate[cafeRoad[mi][mj]] == 0) {
 
-            inputVisited[mi][mj] = 1;
-            inputAte[cafeRoad[mi][mj]] = 1;
-
-            exploreCafe(mi, mj, si, sj, k, inputAte, inputVisited);
-
-            inputVisited[mi][mj] = 0;
-            inputAte[cafeRoad[mi][mj]] = 0;
+            visited[mi][mj] = 1;
+            ate[cafeRoad[mi][mj]] = 1;
+            exploreCafe(mi, mj, si, sj, k);
+            visited[mi][mj] = 0;
+            ate[cafeRoad[mi][mj]] = 0;
         }
 
         if (k < 3) {
-            int temp_k = k + 1;
-            int ti = i + di[temp_k];
-            int tj = j + dj[temp_k];
+            int ti = i + di[k + 1];
+            int tj = j + dj[k + 1];
             if (0 <= ti && ti < N &&
                     0 <= tj && tj < N &&
-                    inputVisited[ti][tj] == 0 &&
-                    inputAte[cafeRoad[ti][tj]] == 0) {
+                    visited[ti][tj] == 0 &&
+                    ate[cafeRoad[ti][tj]] == 0) {
 
-                inputVisited[ti][tj] = 1;
-                inputAte[cafeRoad[ti][tj]] = 1;
-
-                exploreCafe(ti, tj, si, sj, k + 1, inputAte, inputVisited);
-
-                inputVisited[ti][tj] = 0;
-                inputAte[cafeRoad[ti][tj]] = 0;
+                visited[ti][tj] = 1;
+                ate[cafeRoad[ti][tj]] = 1;
+                exploreCafe(ti, tj, si, sj, k + 1);
+                visited[ti][tj] = 0;
+                ate[cafeRoad[ti][tj]] = 0;
             }
         }
     }
